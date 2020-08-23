bc41f71c5a2d2def729d689ec4e6f6781e5ef3da
BenYang68 <50181923+BenYang68@users.noreply.github.com>
2020-08-22 22:18:17 -0700

    first commit

---
+++ Sorting Visualizer/src/Bars.java
@@ -0,0 +1,26 @@
+
+public class Bars {
+	
+	private int x, y , width, height;
+	
+	public Bars(int x, int y, int height, int width) {
+		this.x=x;
+		this.y=y;
+		this.height =height;
+		this.width=width;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+
+}
---
+++ Sorting Visualizer/src/InsertionSort.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class InsertionSort {
+	
+	int size;
+	ArrayList<Bars> list1;
+	int index=0;
+	
+	public InsertionSort(ArrayList<Bars> rec, int size) {
+		this.size=size;
+		list1 = rec;
+	}
+	
+	public int nextStep() {
+		 
+
+		//System.out.println(index);
+		if(index <size) {
+
+			Bars b = list1.get(index);
+			
+			for( int i =index-1; i>=0 ; i--) {
+				if(i==0 && list1.get(index).getHeight()<list1.get(i).getHeight()) {
+					list1.remove(index);
+					list1.add(0,b);
+				}
+					
+					
+					
+				if(list1.get(i).getHeight() <= list1.get(index).getHeight()) {
+					list1.remove(index);
+					list1.add(i+1,b);
+					break;
+				}
+			}
+			
+			
+			index++;
+			return 1;
+		}
+		
+		
+		return -1;
+	}
+	
+	
+	public ArrayList<Bars> getlist(){
+		
+		return list1;
+	}
+	
+
+}
